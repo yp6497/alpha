10ab6f358960819ff08579bea33493283fba57e1
yp6497 <59597619+yp6497@users.noreply.github.com>
2021-12-23 20:32:57 +0200

    aut gallery not

---
+++ app/src/main/java/com/example/alpha/not.java
@@ -0,0 +1,34 @@
+package com.example.alpha;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+ class help extends Application {
+
+    public static final String CHANNEL_ID = "channel";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+        createNotificationChannels();
+    }
+
+
+    private void createNotificationChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Channel",
+                    NotificationManager.IMPORTANCE_LOW
+            );
+
+            channel.setDescription("This is notificashion ");
+            NotificationManager m = getSystemService(NotificationManager.class);
+            m.createNotificationChannel(channel);
+        }
+    }
+}

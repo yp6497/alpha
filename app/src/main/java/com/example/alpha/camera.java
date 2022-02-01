25f411d6580def43d076aa74fe8371fef38b0753
yp6497 <59597619+yp6497@users.noreply.github.com>
2022-02-01 02:04:24 +0200

    aut gallery not camera

--- app/src/main/java/com/example/alpha/camera.java
+++ app/src/main/java/com/example/alpha/camera.java
@@ -54,7 +54,7 @@ public class camera extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); //
         setContentView(R.layout.activity_camera);
 
         /*
